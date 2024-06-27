4a33e2a6131ff1bac30cf5fa24683bd69840f5f3
michalmillsivan <michalmills20@gmail.com>
2024-06-27 13:44:10 +0300

    i did itt!!! it collapse!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mainContent = document.querySelector("#main-content")
 const API_URL = "https://api.coingecko.com/api/v3/coins/list"
+const API_URL_ID = "https://api.coingecko.com/api/v3/coins"
 
 function init() {
 
@@ -59,12 +60,12 @@ function drawCoins(coins) {
     if (!Array.isArray(coins)) return;
 
     const coinCards = coins.map(_createCoinCard)
-    function _createCoinCard(coin) {
+    function _createCoinCard(coin, index) {
         const cardCol = document.createElement("div")
         cardCol.classList.add("col-md-4")
 
         const cardContent = document.createElement("div")
-        cardContent.classList.add("card")
+        cardContent.classList.add("card", "flex")
 
         const cardBody = document.createElement("div")
         cardBody.classList.add("card-body")
@@ -77,13 +78,61 @@ function drawCoins(coins) {
         cardText.classList.add("card-text")
         cardText.innerText = coin?.name;
 
-        const cardBtn = document.createElement("a")
-        // cardBtn.classList.add("btn btn-primary")
-        // cardBody.href = "#XXXXXX"
+        const cardBtn = document.createElement("button")
+        cardBtn.classList.add("btn", "btn-primary")
+        cardBtn.setAttribute("data-toggle", "collapse")
+        cardBtn.setAttribute("href", `#coin.id`)
+        cardBtn.setAttribute("role", "button")
+        cardBtn.setAttribute("aria-expanded", "false")
         cardBtn.innerHTML = "More Info"
 
+        const additionalInfo = document.createElement("div")
+        additionalInfo.setAttribute("id", `coin.id`)
+        additionalInfo.classList.add("additional-info", "collapse")
+
+        const coinImg = document.createElement("div")
+        coinImg.classList.add("coin_image")
+        const coinPrice = document.createElement("div")
+
+        cardBtn.addEventListener("click", async () => {
+            try {
+                const coinData = await getCoinsApi(coin.id)
+                coinImg.innerHTML = ''
+                coinPrice.innerHTML = ''
+
+                if (coinData.image && coinData.image.small) {
+                    const coinImage = document.createElement("img")
+                    coinImage.src = coinData.image.small
+                    coinImg.appendChild(coinImage)
+                }
+
+                if (coinData.market_data && coinData.market_data.current_price) {
+                    const priceUSD = document.createElement("p");
+                    priceUSD.innerText = `Price (USD): ${coinData.market_data.current_price.usd}`;
+                    coinPrice.appendChild(priceUSD)
+
+                    const priceEUR = document.createElement("p");
+                    priceEUR.innerText = `Price (EUR): ${coinData.market_data.current_price.eur}`;
+                    coinPrice.appendChild(priceEUR)
+
+                    const priceILS = document.createElement("p");
+                    priceILS.innerText = `Price (ILS): ${coinData.market_data.current_price.ils}`;
+                    coinPrice.appendChild(priceILS)
+
+                }
+
+                const expanded = cardBtn.getAttribute("aria-expanded") === "true"
+                cardBtn.setAttribute("aria-expanded", String(!expanded))
+                additionalInfo.classList.toggle("show")
+
+            } catch (error) {
+                console.error("Error fetching coin data:", error)
+            }
+        })
+
         cardBody.append(cardTitle, cardText, cardBtn)
-        cardContent.append(cardBody)
+        additionalInfo.append(coinImg, coinPrice)
+        cardContent.append(cardBody, additionalInfo)
         cardCol.append(cardContent)
 
         return cardCol;
@@ -92,14 +141,13 @@ function drawCoins(coins) {
 
 }
 
-async function getCoinsApi() {
+async function getCoinsApi(id) {
     try {
-        const response = await fetch(API_URL, {method: "GET"})
+        const response = await fetch(id ? API_URL_ID + "/" + id : API_URL, { method: "GET" })
         if (!response.ok) {
             throw new Error('Failed to fetch data');
         }
         const coins = await response.json();
-        console.log(coins)
         return coins;
     } catch (error) {
         console.error('Error fetching API:', error);
@@ -108,16 +156,16 @@ async function getCoinsApi() {
 }
 
 const coinArray = [
-    {"id":"dogecoin","symbol":"doge","name":"Dogecoin"},
-    {"id":"litecoin","symbol":"ltc","name":"Litecoin"},
-    {"id": "bitcoin","symbol": "btc","name": "Bitcoin"},
-    {"id":"peercoin","symbol":"ppc","name":"Peercoin"},
-    {"id":"auroracoin","symbol":"aur","name":"Auroracoin"},
-    {"id":"intergalactic","symbol":"🐒","name":"INTERGALACTIC"},
-    {"id":"nxt","symbol":"nxt","name":"NXT"},
-    {"id":"counterparty","symbol":"xcp","name":"Counterparty"},
-    {"id":"omni","symbol":"omni","name":"Omni"},
-    {"id":"namecoin","symbol":"nmc","name":"Namecoin"}   
+    { "id": "dogecoin", "symbol": "doge", "name": "Dogecoin" },
+    { "id": "litecoin", "symbol": "ltc", "name": "Litecoin" },
+    { "id": "bitcoin", "symbol": "btc", "name": "Bitcoin" },
+    { "id": "peercoin", "symbol": "ppc", "name": "Peercoin" },
+    { "id": "auroracoin", "symbol": "aur", "name": "Auroracoin" },
+    { "id": "intergalactic", "symbol": "🐒", "name": "INTERGALACTIC" },
+    { "id": "nxt", "symbol": "nxt", "name": "NXT" },
+    { "id": "counterparty", "symbol": "xcp", "name": "Counterparty" },
+    { "id": "omni", "symbol": "omni", "name": "Omni" },
+    { "id": "namecoin", "symbol": "nmc", "name": "Namecoin" }
 ]
 
 init()
